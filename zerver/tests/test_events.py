d43b20d72d8446217e30143a5235d725fcb79e8f
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-01-03 03:04:28 +0530

    email_pill: Add email_extraction_regex to extract email addresses.
    This commit adds logic to convert email addresses with names attached
    into invite users email input to pills.
    
    Fixes: #32874.

--- zerver/tests/test_events.py
+++ zerver/tests/test_events.py
@@ -1234,7 +1234,7 @@ class NormalActionsTest(BaseAction):
         with self.verify_action(state_change_expected=False) as events:
             do_invite_users(
                 self.user_profile,
-                ["foo@zulip.com"],
+                [("foo@zulip.com", "")],
                 streams,
                 include_realm_default_subscriptions=False,
                 invite_expires_in_minutes=invite_expires_in_minutes,
@@ -1266,7 +1266,7 @@ class NormalActionsTest(BaseAction):
         with self.captureOnCommitCallbacks(execute=True):
             do_invite_users(
                 user_profile,
-                ["foo@zulip.com"],
+                [("foo@zulip.com", "")],
                 [],
                 include_realm_default_subscriptions=False,
                 invite_expires_in_minutes=invite_expires_in_minutes,
@@ -1289,7 +1289,7 @@ class NormalActionsTest(BaseAction):
         with self.captureOnCommitCallbacks(execute=True):
             do_invite_users(
                 self.user_profile,
-                ["foo@zulip.com"],
+                [("foo@zulip.com", "")],
                 streams,
                 include_realm_default_subscriptions=False,
                 invite_expires_in_minutes=invite_expires_in_minutes,
@@ -1335,7 +1335,7 @@ class NormalActionsTest(BaseAction):
         with self.captureOnCommitCallbacks(execute=True):
             do_invite_users(
                 self.user_profile,
-                ["foo@zulip.com"],
+                [("foo@zulip.com", "")],
                 streams,
                 include_realm_default_subscriptions=False,
                 invite_expires_in_minutes=invite_expires_in_minutes,
